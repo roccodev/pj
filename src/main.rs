2ddfde4c3f58857f0e9d1325f19757e3e3632278
RoccoDev <roccodev.business@gmail.com>
2019-03-04 19:44:21 +0100

    Added files

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+// Copyright (C) 2019 RoccoDev
+// 
+// This file is part of pj.
+// 
+// pj is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// pj is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with pj.  If not, see <http://www.gnu.org/licenses/>.
+
+extern crate termion;
+
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+use termion::{color, style};
+use std::io::{Write, stdout, stdin};
+fn main() {
+ 
+}
